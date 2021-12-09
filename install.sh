f8efb4c6894701f00759fe990c03420c1fe900d1
khousuylong <khou.suylong@gmail.com>
2021-12-09 10:18:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y neovim fd-find
+apt-get install -y neovim fd-find xclip
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
